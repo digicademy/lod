e39f35b8cf8a6345f1b9c4d25397ef69a883ae53
metacontext <Torsten.Schrade@adwmainz.de>
2020-01-19 08:04:41 +0100

    Fixed html/rdfa, ttl and nt templates for TYPO3 9.5

--- ext_localconf.php
+++ ext_localconf.php
@@ -78,6 +78,7 @@ if (TYPO3_MODE === 'BE') {
     ];
 }
 
+// register signal/slot for ItemMappingService
 $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
 $signalSlotDispatcher->connect(
   TYPO3\CMS\Extbase\Persistence\Generic\Mapper\DataMapper::class,
@@ -85,3 +86,8 @@ $signalSlotDispatcher->connect(
   Digicademy\Lod\Service\ItemMappingService::class,
   'mapGenericProperty'
 );
+
+// exclude extension parameters from cHash generation
+$GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'tx_lod_api[iri]';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'tx_lod_api[page]';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'tx_lod_api[limit]';
